b08388870767bb15a329a24ba6b5ff233a9e9f5a
amren1254 <yamren00@gmail.com>
2023-12-27 22:57:07 +0530

    added
    details for own resume

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,4 +1,7 @@
 import ConsultlyLogo from "../images/logos/consultly.svg";
+import TCSLogo from "../images/logos/tcs.png";
+import XebiaLogo from "../images/logos/xebia.png";
+import TutreeLogo from "../images/logos/tutree.png";
 import AmbitLogo from "../images/logos/ambit.png";
 import BarepapersLogo from "../images/logos/barepapers.svg";
 import BimLogo from "../images/logos/bim.png";
@@ -20,259 +23,150 @@ import { LinkedInIcon } from "../components/icons/LinkedInIcon";
 import { XIcon } from "../components/icons/XIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Amrendra Yadav",
+  initials: "AY",
+  location: "New Delhi, India",
+  locationLink: "https://www.google.com/maps/place/NewDelhi",
   about:
-    "Full Stack Engineer focused on building products with extra attention to details",
+    "Passionate Software Engineer with 4 years of hands-on experience in designing, implementing, and maintaining scalable and efficient web applications.",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As a Backend Software Engineer, I have successfully completed multiple projects. Currently, I work mostly with Golang, Gin, REST and Databases(MySQL, Psql).",
+  avatarUrl: "",
+  personalWebsiteUrl: "https://amren1254.github.io",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "y.amren00@gmail.com",
+    tel: "+91-8896726484",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/amren1254",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/amren1254/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/i_am_amren",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Institute of Technology and Management, GIDA, Gorakhpur",
+      degree: "B.Tech in Computer Science and Engineering with 69.5%",
+      start: "Aug/2015",
+      end: "June/2019",
+    },
+    {
+      school: "Devasthaly Vidyapeeth Devasthaly, Ballia",
+      degree: "Intermediate(CBSE) with 74.6%",
+      start: "April/2014",
+      end: "June/2015"
+    },
+    {
+      school: "Devasthaly Vidyapeeth Devasthaly, Ballia",
+      degree: "High School(CBSE) with 9.6CGPA",
+      start: "April/2012",
+      end: "March/2013"
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      company: "Xebia IT Infotech, Gurgaon",
+      link: "https://xebia.com/apac/",
+      badges: ["Remote", "Full-Time"],
+      title: "Consultant",
+      logo: XebiaLogo,
+      start: "Feb/2022",
+      end: "Oct/2023",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Implemented new features, unit tests, developed a tool for analysis of daily healthy/unhealthy tx's.\
+        Improved security, optimized code.  Technologies: Golang, Gin, MySQL, Docker, Kafka, Grafana, Git",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      company: "TCS, Pune",
+      link: "https://www.tcs.com/",
+      badges: ["Remote", "Full-Time"],
+      title: "Assistant System Engineer Trainee → System Engineer",
+      logo: TCSLogo,
+      start: "Jan/2020",
+      end: "Jan/2022",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Created desktop app and web apps. Designed architecture on MS Azure.\
+         Technologies: Golang, C#, MySQL, Docker, MS Azure",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      company: "Tutree Inc. , Noida",
+      link: "https://tutree-india-technologies-limited.business.site/",
+      badges: ["Intern"],
+      title: "Software Developer",
+      logo: TutreeLogo,
+      start: "Aug/2019",
+      end: "Nov/2019",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "My Contribution here is to make API’s using gorilla/mux, use data-structure and algorithms\
+        and optimize codebase, write unit test cases and bug fixing",
     },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Golang",
+    "MS Azure Cloud",
+    "DSA",
+    "Python/C#",
+    "Microservices",
+    "Docker",
+    "MySQL/PostgreSQL",
+    "Gin/gorilla-mux",
+    "HTML/CSS",
+    "Git"
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Risk Management Service",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Client-Project/XEBIA",
+        "Golang",
+        "Gin",
+        "Docker",
+        "Grafana",
+        "Kafka",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Microservice to assess the risk occurring during a transaction from the \
+      user and label it as healthy/unhealthy by connecting with an external service provider.",
+      logo: XebiaLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "xebia.com",
+        href: "https://xebia.com/apac",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "DWA",
+      techStack: ["Client-Project/TCS", "Golang", "MS Azure", "Web app", "Docker"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "REST API based web app exposing multiple endpoints for user and vehicle management",
+      logo: TCSLogo,
       link: {
         label: "monito.dev",
-        href: "https://monito.dev/",
+        href: "https://tcs.com/",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "Tutree web app",
+      techStack: ["Tutree", "Golang", "Python", "MySQL", "Docker", "Gorilla/mux", "Testing"],
       description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+        "Tutree is a human intelligence network. A tutor is only a tap away. In-person qualified tutors \
+        will come to your location of choice on demand.\
+        In this application my contribution is to make API’s using gorilla/mux, use data-structure and algorithms\
+        and optimize codebase, write unit test cases and bug fixing.",
+      logo: TutreeLogo,
       link: {
         label: "github.com",
-        href: "https://jarocki.me/",
+        href: "https://tutree.com",
       },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
